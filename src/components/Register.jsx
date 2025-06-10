781ef95d26dbd542f23e50a0e76e46311366a6f9
Marcin Czapla <marcin.czapla.programista@gmail.com>
2025-06-10 21:01:03 +0200

    Add paragraph to input password confirmation to inform if it is corretly filled

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -173,6 +173,28 @@ const StyledFontAwesomeIconInvalidPasswordConfirm = styled(FontAwesomeIcon)`
     $validMatch || !$matchPwd ? "0.25rem" : ""};
 `;
 
+const ParagraphPasswordConfrim = styled.p`
+  position: ${({ $validMatch, $matchFocus }) =>
+    $matchFocus && !$validMatch ? "relative" : "absolute"};
+  left: ${({ $validMatch, $matchFocus }) =>
+    $matchFocus && !$validMatch ? "" : "9999px"};
+  font-size: ${({ $validMatch, $matchFocus }) =>
+    $matchFocus && !$validMatch ? "0.75rem" : ""};
+  border-radius: ${({ $validMatch, $matchFocus }) =>
+    $matchFocus && !$validMatch ? "0.5rem" : ""};
+  background: ${({ $validMatch, $matchFocus }) =>
+    $matchFocus && !$validMatch ? "#000" : ""};
+  color: ${({ $validMatch, $matchFocus }) =>
+    $matchFocus && !$validMatch ? "#fff" : ""};
+  padding: ${({ $validMatch, $matchFocus }) =>
+    $matchFocus && !$validMatch ? "0.25rem" : ""};
+  bottom: ${({ $validMatch, $matchFocus }) =>
+    $matchFocus && !$validMatch ? "-10px" : ""};
+  svg {
+    margin-right: 0.25rem;
+  }
+`;
+
 const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
 const EMAIL_REGEX = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
@@ -381,6 +403,14 @@ function Register() {
               onFocus={() => setMatchFocus(true)}
               onBlur={() => setMatchFocus(false)}
             />
+            <ParagraphPasswordConfrim
+              id="confirmnote"
+              $matchFocus={matchFocus}
+              $validMatch={validMatch}
+            >
+              <FontAwesomeIcon icon={faInfoCircle} />
+              Must match the first password input field.
+            </ParagraphPasswordConfrim>
           </FormWrapper>
         </SectionWrapper>
       )}

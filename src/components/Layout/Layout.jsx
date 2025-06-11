207dc7193b15f022367aeccb307111f5e608cfd1
Marcin Czapla <marcin.czapla.programista@gmail.com>
2025-06-11 12:31:02 +0200

    Add styles to Layout.jsx

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,21 @@
+import { Outlet } from "react-router-dom";
+import styled from "styled-components";
+
+const WrapperApp = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  padding: 1rem 0.5rem;
+`;
+
+const Layout = () => {
+  return (
+    <WrapperApp className="App">
+      <Outlet />
+    </WrapperApp>
+  );
+};
+
+export default Layout;

b7f089ff15b571a5ffd7a22ad19b2a4333ee1fc5
Marcin Czapla <marcin.czapla.programista@gmail.com>
2025-06-11 08:07:11 +0200

    Add state for email, password, error

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,6 +3,10 @@ import { useRef, useState, useEffect, useContext } from "react";
 const Login = () => {
   const userRef = useRef();
   const errRef = useRef();
+
+  const [email, setEmail] = useState("");
+  const [pwd, setPwd] = useState("");
+  const [errMsg, setErrMsg] = useState("");
   return <div></div>;
 };
 

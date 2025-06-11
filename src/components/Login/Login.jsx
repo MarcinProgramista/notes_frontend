3a4af8520096f1a7dfa9108f0c55467fd24a439d
Marcin Czapla <marcin.czapla.programista@gmail.com>
2025-06-11 08:16:30 +0200

    Replace div with section element - styled

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,4 +1,16 @@
 import { useRef, useState, useEffect, useContext } from "react";
+import styled from "styled-components";
+
+const SectionWrapper = styled.section`
+  width: 100%;
+  max-width: 420px;
+  min-height: 400px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  padding: 1rem;
+  background-color: rgba(0, 0, 0, 0.4);
+`;
 
 const Login = () => {
   const userRef = useRef();
@@ -13,7 +25,7 @@ const Login = () => {
     setErrMsg("");
   }, [email, pwd]);
 
-  return <div></div>;
+  return <SectionWrapper></SectionWrapper>;
 };
 
 export default Login;

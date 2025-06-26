4bd82a1c91d79cf47c4bfa765562bdf1bb4631d6
Marcin Czapla <marcin.czapla.programista@gmail.com>
2025-06-26 16:20:16 +0200

    Add controler getCategoryNotes

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 
 const Notes = () => {
+  const [notes, setNotes] = useState();
   const params = useParams();
   const category_id = params.category_id;
   return <div>{category_id}</div>;

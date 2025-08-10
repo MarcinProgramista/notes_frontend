c9e93e1507158cf0de5a6a9fa59a9f60ae1ff164
Marcin Czapla <marcin.czapla.programista@gmail.com>
2025-08-10 20:59:43 +0200

    Add chaneges in css to component ButtonIcon.jsx

--- src/components/ButtonIcon/ButtonIcon.jsx
+++ src/components/ButtonIcon/ButtonIcon.jsx
@@ -7,10 +7,37 @@ const ButtonIcon = styled.button`
   border-radius: 20px;
   background-repeat: no-repeat;
   background-position: 50% 50%;
-  background-size: 40%;
+  background-size: 70%;
   background-image: url(${({ $icon }) => $icon});
   background-color: white;
-  border: ${({ $active }) => ($active ? "4px" : "1px")};
+
+  ${({ $active, $category }) =>
+    $active &&
+    $category === "Books" &&
+    css`
+      border: 6px solid hsl(106, 47%, 64%);
+      background-size: 80%;
+      width: 70px;
+      height: 70px;
+    `}
+  ${({ $active, $category }) =>
+    $active &&
+    $category === "Notes" &&
+    css`
+      border: 6px solid #ffd82b;
+      background-size: 80%;
+      width: 70px;
+      height: 70px;
+    `}
+    ${({ $active, $category }) =>
+    $active &&
+    $category === "Films" &&
+    css`
+      border: 8px solid hsl(196, 83%, 75%);
+      background-size: 80%;
+      width: 70px;
+      height: 70px;
+    `}
 `;
 
 export default ButtonIcon;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -14,6 +14,7 @@ import styled, { css } from "styled-components";
 import Books from "../../assets/books-svgrepo-com.png";
 import Films from "../../assets/video-svgrepo-com.png";
 import Notes from "../../assets/pen-nib-svgrepo-com.png";
+import ButtonIcon from "../ButtonIcon/ButtonIcon";
 
 const StyledNavbar = styled.div`
   display: flex;
@@ -163,44 +164,7 @@ const StyledParagraph = styled.div`
       color: hsl(106, 47%, 64%);
     `}
 `;
-const ButtonIcon = styled.button`
-  width: 60px;
-  height: 60px;
-  border-radius: 20px;
-  background-repeat: no-repeat;
-  background-position: 50% 50%;
-  background-size: 70%;
-  background-image: url(${({ $icon }) => $icon});
-  background-color: white;
 
-  ${({ $active, $category }) =>
-    $active &&
-    $category === "Books" &&
-    css`
-      border: 6px solid hsl(106, 47%, 64%);
-      background-size: 80%;
-      width: 70px;
-      height: 70px;
-    `}
-  ${({ $active, $category }) =>
-    $active &&
-    $category === "Notes" &&
-    css`
-      border: 6px solid #ffd82b;
-      background-size: 80%;
-      width: 70px;
-      height: 70px;
-    `}
-    ${({ $active, $category }) =>
-    $active &&
-    $category === "Films" &&
-    css`
-      border: 8px solid hsl(196, 83%, 75%);
-      background-size: 80%;
-      width: 70px;
-      height: 70px;
-    `}
-`;
 const Home = () => {
   const { auth, setAuth } = useContext(AuthContext);
   const navigate = useNavigate();

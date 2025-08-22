32da32f3eed202775eaa1afa4d3a353bde31d5a4
Marcin Czapla <marcin.czapla.programista@gmail.com>
2025-08-22 19:44:12 +0200

    Add StyledWrapper to NewNoteItem.jsx

--- src/components/NewNoteItem/NewNoteItem.jsx
+++ src/components/NewNoteItem/NewNoteItem.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import styled from "styled-components";
 
+const StyledWrapper = styled.div`
+  border-left: 10px solid ${({ theme, activecolor }) => theme[activecolor]};
+  z-index: 9999;
+  position: fixed;
+  display: flex;
+  padding: 100px 90px;
+  flex-direction: column;
+  right: 0;
+  top: 0;
+  height: 100vh;
+  width: 680px;
+  background-color: white;
+  box-shadow: -5px 0 15px rgba(0, 0, 0, 0.1);
+`;
 const NewNoteItem = () => {
-  return <div>hello</div>;
+  return <StyledWrapper>hello</StyledWrapper>;
 };
 
 export default NewNoteItem;
